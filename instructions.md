4d4ea2d51048dd75c509f40d243e14358c4e9e0f
vggadde <vggadde@github.com>
2020-07-29 23:00:40 -0400

    working state

--- instructions.md
+++ instructions.md
@@ -1,2 +1,12 @@
+Pre-requisites
+1. Terraform v0.12.29
+2. aws-cli/1.18.69 Python/3.6.9 Linux/4.15.0-112-generic botocore/1.16.19
+3. ansible 2.5.1
+
 1. Update the public key logic in main.tf
 2. aws configure --profile superhero
+3. Navigate to folder and execute ansible-playbook install-basics.yml 
+4. CHange myIP in terraform secrets
+5. Several warning on security in jenkins server.. ignoring for now, but should be fixed in production.
+6. docker compose should run in directory where docker-compose fils is in. 
+7. adjust permissions for kryptonite private key after
--- main.tf
+++ main.tf
@@ -8,76 +8,76 @@ provider "aws" {
 
 #-------------VPC-----------
 
-resource "aws_vpc" "springpc_vpc" {
+resource "aws_vpc" "softram_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "springpc_vpc"
+    Name = "softram_vpc"
   }
 }
 
-#internet gateway
+#-------------Internet Gateway-------------
 
-resource "aws_internet_gateway" "springpc_internet_gateway" {
-  vpc_id = aws_vpc.springpc_vpc.id
+resource "aws_internet_gateway" "softram_internet_gateway" {
+  vpc_id = aws_vpc.softram_vpc.id
 
   tags = {
-    Name = "springpc_igw"
+    Name = "softram_igw"
   }
 }
 
-# Route tables
+#-------------Route tables-------------
 
-resource "aws_route_table" "springpc_public_rt" {
-  vpc_id = aws_vpc.springpc_vpc.id
+resource "aws_route_table" "softram_public_rt" {
+  vpc_id = aws_vpc.softram_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.springpc_internet_gateway.id
+    gateway_id = aws_internet_gateway.softram_internet_gateway.id
   }
 
   tags = {
-    Name = "springpc_public"
+    Name = "softram_public"
   }
 }
 
-resource "aws_default_route_table" "springpc_private_rt" {
-  default_route_table_id = aws_vpc.springpc_vpc.default_route_table_id
+resource "aws_default_route_table" "softram_private_rt" {
+  default_route_table_id = aws_vpc.softram_vpc.default_route_table_id
 
   tags = {
-    Name = "springpc_private"
+    Name = "softram_private"
   }
 }
 
-resource "aws_subnet" "springpc_public1_subnet" {
-  vpc_id                  = aws_vpc.springpc_vpc.id
+#-------------Subnets-------------
+
+resource "aws_subnet" "softram_public1_subnet" {
+  vpc_id                  = aws_vpc.softram_vpc.id
   cidr_block              = var.cidrs["public1"]
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name = "springpc_public1"
+    Name = "softram_public1"
   }
 }
 
 
-#
+#-------------Subnet Associations-------------
 
-# Subnet Associations
-
-resource "aws_route_table_association" "springpc_public_assoc" {
-  subnet_id      = aws_subnet.springpc_public1_subnet.id
-  route_table_id = aws_route_table.springpc_public_rt.id
+resource "aws_route_table_association" "softram_public_assoc" {
+  subnet_id      = aws_subnet.softram_public1_subnet.id
+  route_table_id = aws_route_table.softram_public_rt.id
 }
 
-#Security groups
+#-------------Security groups-------------
 
-resource "aws_security_group" "springpc_jenk_sg" {
-  name        = "springpc_jenk_sg"
+resource "aws_security_group" "softram_jenk_sg" {
+  name        = "softram_jenk_sg"
   description = "Used for access to the dev instance"
-  vpc_id      = aws_vpc.springpc_vpc.id
+  vpc_id      = aws_vpc.softram_vpc.id
 
   #SSH
 
@@ -88,23 +88,10 @@ resource "aws_security_group" "springpc_jenk_sg" {
     cidr_blocks = [var.localip]
   }
 
-  #HTTP
-
+  #Jenkins container will be accessible here
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = [var.localip]
-  }
-    ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = [var.localip]
-  }
-    ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [var.localip]
   }
@@ -117,7 +104,7 @@ resource "aws_security_group" "springpc_jenk_sg" {
   }
 }
 
-#-----JENK SERVER--------
+#-------------EC2 instance to host Jenkins container-------------
 
 #key pair
 
@@ -126,24 +113,25 @@ resource "aws_key_pair" "wp_auth" {
 	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCq5bqH/YT4BT//zqkcstJHOajtZXpS+2xKQau0XbaN5JTBjLNm9dxUrUdvBSghJVxenJqzAQrFJnhXZ9RIX8welMSdbN4EC9545VUxwONWwYGKl66lZF/gITUOv9s1QIGIKCDVHxz6HgCEulrsjkrcwRy/HNhGMOpQ9I1DGjpq454ZC3mSAFQrYE+Q7vcFa6KfHQYnncBKFlSlb5UMmC6ckxgkq/xtE3ZGk9JdUynBpOahDFn0EmWd7d5kQDoeD0+mGxI9vF8ZgkGZQv/JgYyeN0Zlb+FOTcRjwhqUXY4c6auEQXkzAUJol3JHIqsR1vtHgYjD/4fr6E3U4j/nXCLp v@v-VirtualBox"	
 }
 
-resource "aws_instance" "springpc_jenk" {
+resource "aws_instance" "softram_jenk" {
   instance_type = var.dev_instance_type
   ami           = var.dev_ami
 
   tags = {
-    Name = "springpc_jenk"
+    Name = "softram_jenk"
   }
 
   key_name               = aws_key_pair.wp_auth.id
-  vpc_security_group_ids = [aws_security_group.springpc_jenk_sg.id]
+  vpc_security_group_ids = [aws_security_group.softram_jenk_sg.id]
   #iam_instance_profile   = aws_iam_instance_profile.s3_access_profile.id
-  subnet_id              = aws_subnet.springpc_public1_subnet.id
+  subnet_id              = aws_subnet.softram_public1_subnet.id
 
+#This is used in ansible configuration
     provisioner "local-exec" {
     command = <<EOD
 cat <<EOF > jenk-host
 [jenk] 
-${aws_instance.springpc_jenk.public_ip} 
+${aws_instance.softram_jenk.public_ip} 
 EOF
 EOD
 }
